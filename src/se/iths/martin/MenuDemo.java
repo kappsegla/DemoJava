03dc6cbedc381ebce78ddc71c6fe13f14a6d5bf2
Martin Blomberg <martin.blomberg@outlook.com>
2018-08-28 13:41:10 +0200

    for loops

--- src/se/iths/martin/MenuDemo.java
+++ src/se/iths/martin/MenuDemo.java
@@ -5,33 +5,56 @@ import java.util.Scanner;
 
 public class MenuDemo {
 
+
+
+
+
     public static void printMenu() {
-        System.out.println("1. Alternativ 1");
-        System.out.println("2. Alternativ 2");
-        System.out.println("3. abc...");
+
+//        String[] menuOptions = new String[4];
+//        menuOptions[0] = "1. Alternativ 1";
+//        menuOptions[1] = "2. Alternativ 2";
+//        menuOptions[2] = "3. abc...";
+//        menuOptions[3] = "0. Avsluta";
+//        for (int i = 0; i < menuOptions.length; i++) {
+//            System.out.println(menuOptions[i]);
+//        }
+
+         String[] menuOptions = new String[] {
+                 "1. Alternativ 1","2. Alternativ 2",
+                 "3. abc...","0. Avsluta" };
+
+        for ( String s : menuOptions ) {
+            System.out.println(s);
+        }
+
+        menuOptions[4] = "Hej";
+
     }
 
     public static void main(String[] args) {
-        printMenu();
-        System.out.print("Gör ett val: ");
-
+        String val;
         Scanner sc = new Scanner(System.in);
-        String val = sc.nextLine();
 
-        switch (val) {
-            case "1":
-                menyAlternativ1();
-                break;
-            case "2":
-                menyAlternativ2();
-                break;
-            case "3":
-                menyAlternativ3();
-                break;
-            default:
-                break;
+        do {
+            printMenu();
+            System.out.print("Gör ett val: ");
+            val = sc.nextLine();
 
-        }
+            switch (val) {
+                case "1":
+                    menyAlternativ1();
+                    break;
+                case "2":
+                    menyAlternativ2();
+                    break;
+                case "3":
+                    menyAlternativ3();
+                    break;
+                default:
+                    break;
+            }
+        } while (!val.equals("0"));
     }
 
     private static void menyAlternativ3() {
@@ -39,19 +62,44 @@ public class MenuDemo {
             System.out.println("Tecken: " + i);
         }
 
+        char i = 'a';
+        while (i <= 'z') {
+            System.out.println("Tecken: " + i);
+            i++;
+        }
     }
 
     public static void menyAlternativ2() {
         System.out.println("Alternativ 2 valdes");
-        for (int i = 10; i >= 0; i--) {
-            System.out.println(i);
+
+        int[] myArray = new int[10];
+
+        myArray[0] = 10;
+        myArray[1] = 9;
+        myArray[2] = 8;
+        myArray[3] = 7;
+        myArray[4] = 6;
+        myArray[5] = 5;
+        myArray[6] = 4;
+        myArray[7] = 3;
+        myArray[8] = 2;
+        myArray[9] = 1;
+        myArray[10] = 0;
+
+        for (int i = 0; i < 11; i++) {
+            System.out.println( myArray[i] );
         }
 
+
+
+//        for (int i = 10; i >= 0; i--) {
+//            System.out.println(i);
+//        }
     }
 
     private static void menyAlternativ1() {
         System.out.println("Alternativ 1 valdes.");
-        for(int i = 10;  i < 100 ; i +=10 ){
+        for (int i = 10; i < 100; i += 10) {
             System.out.println(i);
         }
     }
