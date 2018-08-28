bb83994f91183ab9a7cf79517aff1fb54b5d2819
Martin Blomberg <martin.blomberg@outlook.com>
2018-08-28 09:40:13 +0200

    Meny utskrift

--- src/se/iths/martin/MenuDemo.java
+++ src/se/iths/martin/MenuDemo.java
@@ -1,14 +1,25 @@
 package se.iths.martin;
 
+
+import java.util.Scanner;
+
 public class MenuDemo {
+
+    public static void printMenu(){
+        System.out.println("1. Alternativ 1");
+        System.out.println("2. Alternativ 2");
+    }
+
 
     public static void main(String[] args)
     {
+        printMenu();
+        System.out.print("Gör ett val: ");
+
+        Scanner sc = new Scanner(System.in);
+        String val = sc.nextLine();
 
 
 
     }
-
-
-
 }
