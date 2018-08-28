d5fc16abe16e817aa4ea52a4c8a180a507d461c7
Martin Blomberg <martin.blomberg@outlook.com>
2018-08-28 10:35:08 +0200

    for loops

--- src/se/iths/martin/MenuDemo.java
+++ src/se/iths/martin/MenuDemo.java
@@ -8,8 +8,8 @@ public class MenuDemo {
     public static void printMenu() {
         System.out.println("1. Alternativ 1");
         System.out.println("2. Alternativ 2");
+        System.out.println("3. abc...");
     }
-
 
     public static void main(String[] args) {
         printMenu();
@@ -25,17 +25,34 @@ public class MenuDemo {
             case "2":
                 menyAlternativ2();
                 break;
+            case "3":
+                menyAlternativ3();
+                break;
             default:
                 break;
 
         }
     }
 
+    private static void menyAlternativ3() {
+        for (char i = 'a'; i <= 'z'; i++) {
+            System.out.println("Tecken: " + i);
+        }
+
+    }
+
     public static void menyAlternativ2() {
         System.out.println("Alternativ 2 valdes");
+        for (int i = 10; i >= 0; i--) {
+            System.out.println(i);
+        }
+
     }
 
     private static void menyAlternativ1() {
         System.out.println("Alternativ 1 valdes.");
+        for(int i = 10;  i < 100 ; i +=10 ){
+            System.out.println(i);
+        }
     }
 }
