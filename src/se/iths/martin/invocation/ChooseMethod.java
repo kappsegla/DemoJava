9c9abbebee51a431f7ab27045c74a00066e7a34d
Martin Blomberg <martin.blomberg@outlook.com>
2018-09-11 09:57:26 +0200

    Compiletime method choose

--- src/se/iths/martin/invocation/ChooseMethod.java
+++ src/se/iths/martin/invocation/ChooseMethod.java
@@ -1,7 +1,18 @@
 package se.iths.martin.invocation;
 
 public class ChooseMethod {
+
     public void doSomething(Number n) {
         System.out.println("Numbers");
+    }
+    public void doSomething(Integer i){
+        System.out.println("Integer");
+    }
+    /**
+     * Varargs method will have lowest priority
+     * when overloading is decided
+     */
+    public void doSomething(Integer... i){
+        System.out.println("Vararg");
     }
 }
--- src/se/iths/martin/invocation/MethodChooser.java
+++ src/se/iths/martin/invocation/MethodChooser.java
@@ -1,5 +1,13 @@
 package se.iths.martin.invocation;
 
+
+import java.util.Scanner;
+
+/**
+ * Signature of the method to be invoked is decided at compiletime.
+ * Selection of the right implementation of method is done at runtime.
+ */
+
 public class MethodChooser {
     public static void main(String[] args) {
         ChooseMethod m = new ChooseMethod();
