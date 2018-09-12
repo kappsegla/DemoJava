069e865e24a1b38a70a67f4afff18a31b16dd6bb
Martin Blomberg <martin.blomberg@outlook.com>
2018-09-12 09:24:00 +0200

    Book

---
+++ src/se/iths/martin/sorting/Book.java
@@ -0,0 +1,47 @@
+package se.iths.martin.sorting;
+
+public class Book {
+
+
+    public Book(String title) {
+        this("No author", title);
+    }
+
+    public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+        numberOfBooks++;
+    }
+
+    private static int numberOfBooks;
+
+    public static int getNumberOfBooks(){
+        return numberOfBooks;
+    }
+
+    @Override
+    public String toString() {
+        return author + " - " + title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    private String author;
+    private String title;
+
+
+}
